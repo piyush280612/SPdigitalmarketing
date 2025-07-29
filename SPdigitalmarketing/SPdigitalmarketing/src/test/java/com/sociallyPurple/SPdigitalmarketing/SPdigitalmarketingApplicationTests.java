a5f51462f89ef933ed2683e46a29dd00f171fbb9
Sshivani-12 <shivaniwakde1211@gmail.com>
2025-07-29 13:16:08 +0530

    first commit

---
+++ SPdigitalmarketing/SPdigitalmarketing/src/test/java/com/sociallyPurple/SPdigitalmarketing/SPdigitalmarketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sociallyPurple.SPdigitalmarketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SPdigitalmarketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
